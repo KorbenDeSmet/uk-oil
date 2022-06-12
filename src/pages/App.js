eea47bff4e2cfbd94089a08e8f6df1b0241f0a6d
KorbenDeSmet <korbendesmet@hotmail.com>
2022-06-12 23:32:35 +0200

    Add(configuration): Tailwind, formatting, icons

---
+++ src/pages/App.js
@@ -0,0 +1,17 @@
+import data from "./../data/data.json"
+function App() {
+  console.log(data)
+
+  return (
+    <div className="min-h-screen bg-neutral-800">
+      <div>
+        {/* <div className="bg-white">
+        </div>
+        <div className="bg-[#7bbf36]">
+        </div> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
