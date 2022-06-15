499785e72da0bfcb1cb7b15adfd63c5b34cc2029
KorbenDeSmet <korbendesmet@hotmail.com>
2022-06-15 19:19:09 +0200

    Update(Header.js): Added href to img

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import logo from "./../images/ukoil.png"
 
 const Header = ({ data }) => {
+    const handleClick = () => {
+        window.open("https://nopixel.fandom.com/wiki/UK_Oil");
+    }
+
     return (
-        <div className="flex justify-between p-5 h-20 bg-neutral-900 border-b border-[#7bbf36]">
-            <img src={logo} className="" alt="Green uk oil logo" />
+        <div className="flex justify-between py-5 px-7 h-20 bg-neutral-900 border-b border-[#7bbf36]">
+            <img src={logo} className="hover:cursor-pointer" alt="Green uk oil logo" onClick={() => handleClick()} />
             <div className="my-auto text-lg"><div>Weeks of income: <u>{data.length}</u></div></div>
         </div>
     )
