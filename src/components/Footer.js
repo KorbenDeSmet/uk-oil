8a60b3d7f041b0d601a82fa9e92a21195ce0c786
KorbenDeSmet <korbendesmet@hotmail.com>
2022-06-17 16:23:24 +0200

    Change url in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="flex float-center p-2 bg-neutral-900 lg:w-fit lg:mx-auto lg:px-3 lg:rounded-t-xl">
-            <a href="https://github.com/KorbenDeSmet" className="mx-auto italic text-sm text-gray-400 hover:text-[#7bbf36] hover:underline">Github of the creator</a>
+            <a href="https://github.com/KorbenDeSmet/uk-oil" className="mx-auto italic text-sm text-gray-400 hover:text-[#7bbf36] hover:underline">Link to the repository</a>
         </div>
     )
 }
