6df2217edfb642dcd3177a27a85d04dbb4b9d6cb
KorbenDeSmet <korbendesmet@hotmail.com>
2022-06-17 15:56:53 +0200

    Updated responsiveness

--- src/pages/App.js
+++ src/pages/App.js
@@ -34,7 +34,7 @@ function App() {
     <div className="flex flex-col min-h-screen bg-neutral-800 text-white selection:bg-[#7bbf36] selection:text-black">
       <div className="flex-grow">
         <Header data={data} />
-        <div className="flex flex-wrap px-7 mt-7 justify-between lg:px-72">
+        <div className="flex flex-wrap px-7 mt-7 justify-between lg:mx-auto lg:w-[75%]">
           <div className="flex mb-6 w-full justify-between">
             <div className="flex h-40 flex-col w-[30%] md:grow p-3 shadow-[2px_2px_4px_rgba(169,169,169,0.5)] bg-neutral-900 rounded-md md:mr-9 md:px-5">
               <div className="flex">
@@ -89,25 +89,25 @@ function App() {
               </div>
             </div>
           </div>
-        </div>
-        <div className="flex flex-col grow p-3 lg:px-8 lg:pb-5 mx-7 mb-7 shadow-[2px_2px_4px_rgba(169,169,169,0.5)] bg-neutral-900 rounded-md lg:mt-7 lg:mx-72">
-          <div className="text-xl mx-auto lg:py-2 lg:text-3xl">All transactions</div>
-          <div className="grid grid-cols-7 w-full bg-neutral-800 mt-2 px-2 py-2 rounded-t-lg">
-            <div className="text-center">Week</div>
-            <div className="col-span-2 text-center">Invoice amount</div>
-            <div className="col-span-2 text-center">Profit</div>
-            <div className="col-span-2 text-center">Gallons sold</div>
+          <div className="flex flex-col grow p-3 lg:px-8 lg:pb-5 mb-7 shadow-[2px_2px_4px_rgba(169,169,169,0.5)] bg-neutral-900 rounded-md lg:mt-7 lg:mx-auto lg:w-[75%]">
+            <div className="text-xl mx-auto lg:py-2 lg:text-3xl">All transactions</div>
+            <div className="grid grid-cols-7 w-full bg-neutral-800 mt-2 px-2 py-2 rounded-t-lg">
+              <div className="text-center">Week</div>
+              <div className="col-span-2 text-center">Invoice amount</div>
+              <div className="col-span-2 text-center">Profit</div>
+              <div className="col-span-2 text-center">Gallons sold</div>
+            </div>
+            {
+              reverse_data.map(transaction => (
+                <div className="text-sm text-neutral-400 grid grid-cols-7 w-full px-2 py-2 border border-neutral-800">
+                  <div className="text-center">{transaction.id}</div>
+                  <div className="col-span-2 text-center">{currencyFormat(transaction.invoice_amount)}</div>
+                  <div className="col-span-2 text-white text-center">{currencyFormat((transaction.invoice_amount / 6.25) * 2.75)}</div>
+                  <div className="col-span-2 text-center">{gallonsFormat(transaction.invoice_amount / 6.25)}</div>
+                </div>
+              ))
+            }
           </div>
-          {
-            reverse_data.map(transaction => (
-              <div className="text-sm text-neutral-400 grid grid-cols-7 w-full px-2 py-2 border border-neutral-800">
-                <div className="text-center">{transaction.id}</div>
-                <div className="col-span-2 text-center">{currencyFormat(transaction.invoice_amount)}</div>
-                <div className="col-span-2 text-white text-center">{currencyFormat((transaction.invoice_amount / 6.25) * 2.75)}</div>
-                <div className="col-span-2 text-center">{gallonsFormat(transaction.invoice_amount / 6.25)}</div>
-              </div>
-            ))
-          }
         </div>
       </div>
       <Footer />
