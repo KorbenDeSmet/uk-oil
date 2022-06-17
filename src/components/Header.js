54ed415138a54198ebc8d3e488a65e44a8165fe9
KorbenDeSmet <korbendesmet@hotmail.com>
2022-06-17 15:18:33 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ const Header = ({ data }) => {
     const handleClick = () => {
         window.open("https://nopixel.fandom.com/wiki/UK_Oil");
     }
-
+    //border-[#7bbf36]
     return (
-        <div className="flex justify-between py-5 px-7 h-20 bg-neutral-900 border-b border-[#7bbf36] md:px-16">
+        <div className="flex justify-between py-5 px-7 h-20 bg-neutral-900 border-b md:px-16">
             <img src={logo} className="hover:cursor-pointer" alt="Green uk oil logo" onClick={() => handleClick()} />
             <div className="my-auto text-lg"><div>Weeks of income: <u>{data.length}</u></div></div>
         </div>
