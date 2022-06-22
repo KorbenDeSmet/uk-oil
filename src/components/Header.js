74ee7bfc2f8a1e4bd5462842e8b721fad24c29dd
KorbenDeSmet <korbendesmet@hotmail.com>
2022-06-22 12:13:52 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,9 @@ const Header = ({ data }) => {
 
     return (
         <div className="flex justify-between py-5 px-7 h-20 bg-neutral-900 border-b border-[#7bbf36] md:px-16">
-            <img src={logo} className="hover:cursor-pointer" alt="Green uk oil logo" onClick={() => handleClick()} />
+            <div className="relative w-28">
+                <img src={logo} className="object-contain hover:cursor-pointer" alt="Green uk oil logo" onClick={() => handleClick()} />
+            </div>
             <div className="my-auto text-lg"><div>Weeks of income: <u>{data.length}</u></div></div>
         </div>
     )
